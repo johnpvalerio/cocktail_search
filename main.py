b4d02b3578bd52f22f4edc2b25be2169e024b3dc
John Paulo Valerio <johnp.valerio@gmail.com>
2020-12-29 11:37:56 -0500

    file reading functionality
    main now loops through given file path CLI given arguments. If none given, uses default (example/input.json)

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from resources.thecocktaildb import Cocktail
+import sys
+
+import CocktailSearch
 import json
 
-fpath = 'example/input.json'
-with open(fpath) as f:
-    cInput = json.load(f)['drinks'][0]
-print(cInput)
-c = Cocktail(cInput)
 
-print(c.__dict__)
+def main() -> None:
+    """
+    Main driver function, reads input files and searches for cocktail output
+    If given CLI args, use given paths
+    else use default example
+    :return: None
+    """
+    paths = sys.argv[1:] if len(sys.argv) > 1 else ['example/input.json']
+    # loop through paths
+    for fpath in paths:
+        try:
+            with open(fpath) as f:
+                # load json as dict
+                cInput = json.load(f)['drinks']
+                # loop through drinks in dict
+                for drink in cInput:
+                    CocktailSearch.search(drink)
+        except FileNotFoundError:
+            continue
+
+
+if __name__ == '__main__':
+    main()
